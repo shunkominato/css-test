02333f724ddca4019be90a1a7bc41c010a32f07e
shunkominato <shun19871001@gmail.com>
2022-11-10 23:20:49 +0900

    emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
 import type { ReactElement } from 'react';
 import type { NextPageWithLayout } from './_app';
-import { Box } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 const TopPage: NextPageWithLayout = () => {
+  const Div = styled.div`
+    background-color: tomato;
+    width: 100vw;
+    height: 68px;
+    z-index: 11;
+    border: 1px solid;
+    border-color: #e2e8f0;
+  `;
   return (
     <>
       {[...Array(1000).keys()].map((i) => {
-        return (
-          <Box
-            key={i}
-            height="68px"
-            width="100vw"
-            bg="tomato"
-            zIndex="11"
-            border="1px"
-            borderColor="gray.200"
-          />
-        );
+        return <Div key={i} />;
       })}
     </>
   );
